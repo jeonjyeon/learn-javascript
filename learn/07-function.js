2abb1ab41eff2c2a1b93c31bf9e6db6b06c7ff3b
jeonjyeon <peacheese17@gmail.com>
2025-06-10 16:28:27 +0900

    [study] JavaScript 함수 매개변수 - 함수이름 옆 소괄호() 안에 선언된 변수 - 이런 변수를 특별해 매개변수라고 부름 - 함수 안에서만 접근 가능(지역 변수 같은 느낌) - 함수 호출(실행)할 때 전달한 값이 매개변수 값으로 할당

--- learn/07-function.js
+++ learn/07-function.js
@@ -31,3 +31,31 @@ drawWater();
 // function conti    nue() {}
 // function cla----ss() {}
 // function for() {}
+
+// ------------------------------------------
+
+// 함수 매개변수 (Function Parameters)
+
+// 함수 밖에 있는 변수 = 전역 변수 (golbal scope variables)
+let year = 4027;
+
+// 함수 선언
+function writeDiary(연도, 월, 일) {
+  // 함수 이름 옆 () 선언된 변수들을 특별히 매개변수라고 부른다.
+
+  // 함수 내부에 선언된 변수 = 지역 변수 (local scope variables)
+  let year = 2025;
+  let month = 6;
+  let day = 10;
+
+  // year = 2026;
+
+  // 변수 + 문자 = 문장
+  //  변수('문자') + 변수('문자') = 문자 연결
+  console.log(연도 + '년 ' + 월 + '월 ' + 일 + '일에 일기를 쓴다.');
+}
+
+// 함수 호출
+writeDiary(2025, 6, 10);
+writeDiary(2025, 7, 2);
+writeDiary(2025, 8, 20);
