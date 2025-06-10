4c5396281fc562cfef964032afb5c3a938557592
jeonjyeon <peacheese17@gmail.com>
2025-06-10 13:37:34 +0900

    [study] JavaScript 변수 이름 작성 규칙 - 공백, 숫자, 하이픈, 예약어 사용 X - 의미있는 이름 작성 권장

--- learn/05-variables.js
+++ learn/05-variables.js
@@ -13,13 +13,31 @@
 
 // 처음 가진 사과의 갯수 = 4
 // 변수 선언 및 값 할당
-let 처음_가진_사과의_갯수 = 4;
-console.log('처음_가진_사과의_갯수 = ', 처음_가진_사과의_갯수); // 4
+// let 처음_가진_사과의_갯수 = 4;
+// console.log('처음_가진_사과의_갯수 = ', 처음_가진_사과의_갯수); // 4
+// let initialApples = 4;
+// console.log('initialApples: ', initialApples);
+// let $initialApples = 4;
+// console.log('$initialApples: ', $initialApples);
 
 // 추가로 구매한 사과의 갯수 = 27
-let 추가로_구매한_사과의_갯수 = 27;
-console.log('추가로_구매한_사과의_갯수 = ', 추가로_구매한_사과의_갯수); // 27
+// let 추가로_구매한_사과의_갯수 = 27;
+// console.log('추가로_구매한_사과의_갯수 = ', 추가로_구매한_사과의_갯수); // 27
+// let applesToBuy = 27;
+// console.log('applesToBuy: ', applesToBuy);
 
 // 현재 내가 가진 사과의 갯수 = 처음 가진 사과의 갯수 + 추가로 구매한 사과의 갯수
-let 현재_내가_가진_사과의_갯수 = 처음_가진_사과의_갯수 + 추가로_구매한_사과의_갯수;
-console.log('현재_내가_가진_사과의_갯수 = ', 현재_내가_가진_사과의_갯수); // 4 + 27
+// let 현재_내가_가진_사과의_갯수 = 처음_가진_사과의_갯수 + 추가로_구매한_사과의_갯수;
+// console.log('현재_내가_가진_사과의_갯수 = ', 현재_내가_가진_사과의_갯수); // 4 + 27
+// let total = initialApples + applesToBuy;
+// console.log('total: ', total);
+
+// 잘못된 변수 이름Add commentMore actions
+// 공백(' ')은 안됨.
+// let initial apples [❌]
+// 첫글자가 숫자 안됨.
+// let 4people = '4명의 친구들' [❌]
+// 하이픈(-) 기호 안됨.
+// let dark-impact = '다크 임팩트' [❌]
+// 예약어(예: if, for, while, function, ...) 안됨.
+// let return = '회귀' [❌]
