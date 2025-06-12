f79b23a4f8d12c6b79cec7ff38d84e2738c2b7f3
jeonjyeon <peacheese17@gmail.com>
2025-06-12 11:17:45 +0900

    [study] JavaScript 객체 - 객체 생성 (Literal) - 속성 읽기 (Read) - 속성 쓰기 (Write) - 속성 수정 (Update) - 속성 삭제 (Delete)

---
+++ learn/12-object.js
@@ -0,0 +1,124 @@
+// --------------------------------------------------------------------------
+// 기본 타입(Primitive Types, 간단)
+// --------------------------------------------------------------------------
+
+// 1. String - '문자값'
+// 2. Number - 101
+// 3. Boolean - false
+// 4. null - 비어있다.
+// 5. undefined - 정의되지않음
+// 6. Symbol - Symbol('yamoo9')
+// 7. BigInt - 1000n
+
+// --------------------------------------------------------------------------
+// 객체 타입(Object Types, 복잡)
+// --------------------------------------------------------------------------
+
+// 1. Function - () => {}
+// 2. Object - {}
+// 3. Array - []
+
+// --------------------------------------------------------------------------
+// 📌 객체
+// --------------------------------------------------------------------------
+
+// 객체 생성자(Constructor)를 사용해 객체 생성
+// const 우유 = new Object();
+// 우유.이름 = '저지방 우유';
+// 우유.브랜드 = '서울 우유';
+// 우유.용량 = '1000ml';
+
+// 객체 리터럴을 사용해 객체 정의 (쉬운 방법)
+const 커피 = {
+  용량: '600ml',
+  브랜드: '스타벅스',
+  이름: '아메리카노',
+};
+
+const 국어사전 = {
+  결제: '대금을 주고받아 매매 당사자 사이의 거래 관계를 끝맺는 일',
+  결재: '결정할 권한이 있는 상관이 부하가 제출한 안건을 검토하여 허가하거나 승인',
+};
+
+// console.log(국어사전);
+
+// 노트북을 객체로 만들어 보자!
+const labtop = {
+  장치_이름: 'JYEON',
+  프로세서: 'Intel(R) Core(TM) Ultra 5 125H   3.60 GHz',
+  설치된_RAM: '16.0GB(15.6GB 사용 가능)',
+  시스템_종류: '64비트 운영 체제, x64 기반 프로세서',
+  펜_및_터치: '펜과 10개 터치 포인트 지원',
+};
+// console.log(labtop);
+
+const 객체 = {
+  숫자: 101,
+  문자: '안녕!',
+  불리언: false,
+  널: null,
+  언디파인드: undefined,
+  심볼: Symbol('객체'),
+  빅인트: 910n,
+  배열: [],
+  객체: {},
+  함수: () => {},
+};
+
+const h1Camel = {
+  fontWeight: 200,
+  fontSize: '2.65rem',
+};
+
+// 객체 생성 (리터럴 방법: 쉽다!)
+const h1Kebab = {
+  'font-weight': 200,
+  'font-size': '2.65rem',
+};
+
+// 객체 속성 읽기(Read)
+console.log(h1Kebab['font-weight']); // 200
+
+// 객체 속성 쓰기(Write)
+h1Kebab['font-weight'] = 700; // bold
+h1Kebab['font-style'] = 'oblique';
+
+// 수정된 객체 속성 읽기(Read)
+console.log(h1Kebab['font-weight']); // 700
+console.log(h1Kebab['font-style']); // 'oblique'
+
+console.log(h1Kebab);
+
+// 객체 속성 삭제(Delete)
+delete h1Kebab['font-style'];
+
+console.log(h1Kebab);
+
+// CSS 코드를 JavaScript의 객체로 만들어 보기
+const body = {
+  display: 'flex',
+  flexFlow: 'row nowrap',
+  justifyContent: 'center',
+  alignItems: 'center',
+  afunction: function () {
+    console.log('hi');
+  },
+};
+
+console.log(body);
+
+console.log(body.display);
+console.log(body.flexFlow);
+console.log(body.justifyContent);
+console.log(body.alignItems);
+console.log(body.afunction());
+
+const body2 = {
+  display: 'flex',
+  'flex-flow': 'row nowrap',
+  'justify-content': 'center',
+  'align-items': 'center',
+};
+
+console.log(body2['display']);
+console.log(body2['flex-flow']);
