55233259ec4c5ca9e4101b9da0ea1987fb2565a3
jeonjyeon <peacheese17@gmail.com>
2025-06-16 17:38:15 +0900

    [study] 동기 vs. 비동기 - 동기 처리 (실행 시점이 바로) - 비동기 처리 (실행 시점이 나중에)

--- assets/scripts/learn/28-callbacks.js
+++ assets/scripts/learn/28-callbacks.js
@@ -5,14 +5,41 @@ const button = document.querySelector('button.clickable');
 // 버튼 요소에 이벤트 리스너(함수) 추가
 // 콜백 = 다른 함수에 인수로 저달될 함수(나중에 실행되도록)
 // 이벤트 = 언제 발생할지 알 수 없음 (비동기)
-button.addEventListener('click', () => {
-  console.log('이 함수는 나중에 실행되므로 콜백!');
-});
+// button.addEventListener('click', () => {
+//   console.log('이 함수는 나중에 실행되므로 콜백!');
+// });
 
 // addEventListener()는 한 요소에 이벤트 리스너를 여러 개 추가(N회 가능)
-button.addEventListener('click', () => {
-  console.log('두번째로 연결된 콜백!');
-});
+// button.addEventListener('click', () => {
+//   console.log('두번째로 연결된 콜백!');
+// });
+
+const handleBodyToggleClass = () => {
+  console.log('body');
+  document.body.classList.toggle('is-active');
+};
+
+const handleButtonToggleClass = () => {
+  console.log('button');
+  button.classList.toggle('is-active');
+};
+
+// 버튼 요소에 이벤트 리스너(함수) 추가
+// 콜백 = 다른 함수에 인수로 전달될 함수 (나중에 실행되도록)
+// 이벤트 = 언제 발생할 지 알 수 없음 (비동기 실행)
+button.addEventListener('click', handleBodyToggleClass);
+
+// addEventListener()를 사용해 리스너를 추가하는 것은 x N회 가능
+button.addEventListener('click', handleButtonToggleClass);
+
+// 동기(sync) 실행
+handleBodyToggleClass(); // 1
+handleBodyToggleClass(); // 2
+handleBodyToggleClass(); // 3
+handleBodyToggleClass(); // 4
+handleBodyToggleClass(); // 5
+
+// -------------------------------------------------------
 
 // 콜백함수는 어떻게 보면 이중 중첩 함수인 셈인가?
 //  => 아니다!
@@ -28,25 +55,22 @@ button.addEventListener('click', () => {
 // }
 
 // 콜백 예시
-fn1(fn2);
+// fn1(fn2);
 
-function fn1(callback) {
-  // [1]
-  console.log('fn1');
-  callback();
-}
+// function fn1(callback) {
+//   // [1]
+//   console.log('fn1');
+//   callback();
+// }
 
-function fn2() {
-  // [2]
-  console.log('fn2');
-}
+// function fn2() {
+//   // [2]
+//   console.log('fn2');
+// }
 
 // 레거시 이벤트 vs 모던 이벤트 모델
-// 아래의 코드는 결국 마지막의 것만 실행되는 것처럼 보인다.
-// 왜? 변수에는 하나의 값만 담길 수 있기 때문에 처음 값이 담겼다가 마지막 값이 담겨서 결국엔 마지막 값만 보임.
-button.onclick = function () {
-  document.body.classList.toggle('is-active');
-};
-button.onclick = function () {
-  document.button.classList.toggle('is-active');
-};
+// // button.onclick에 새로운 함수를 할당하면 기존 함수는 덮어쓰기 되어 사라짐.
+// 따라서 여러 개를 연결할 수 없음 → 마지막에 할당된 함수만 실행됨.
+// button.onclick = function () {
+//   document.body.classList.toggle('is-active');
+// };
