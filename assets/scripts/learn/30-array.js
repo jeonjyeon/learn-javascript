9ed0cb2a2dba1377ea3f5ed7ddd9845aed1e4361
jeonjyeon <peacheese17@gmail.com>
2025-06-18 13:09:45 +0900

    [practice] JavaScript 배열(Array) - 배열 생성 - 배열 값 읽기 - 배열 값 쓰기

--- assets/scripts/learn/30-array.js
+++ assets/scripts/learn/30-array.js
@@ -6,17 +6,17 @@
 const 장바구니 = ['대파', '계란', '두부', '시금치', '양파', '케첩'];
 console.log('장바구니 =', 장바구니);
 
-// const 장바구니_항목_개수 = 장바구니.length;
-// console.log(장바구니_항목_개수);
+const 장바구니_항목_갯수 = 장바구니.length;
+console.log('장바구니_항목_갯수 =', 장바구니_항목_갯수);
 
 // 리터럴 방식 말고, 정석적인 방법으로 배열 객체 생성
-// const numbers = new Array('one', 'two', 'three', 'four', 'five')
-// const numbersEn = Array('one', 'two', 'three', 'four', 'five')Add commentMore actions
-// console.log(numbersEn, numbersEn.length)
+const numbers = new Array('one', 'two', 'three', 'four', 'five');
+const numbersEn = Array('one', 'two', 'three', 'four', 'five');
+console.log(numbersEn, numbersEn.length);
 
 // new Array()를 사용해 배열 객체를 생성할 때 발생할 수 있는 문제점
-// const messageList = ['hello', 'good morning'] // 2
-// console.log(messageList)
+const messageList = ['hello', 'good morning']; // 2
+console.log(messageList);
 
 // const numbers = new Array(1, 2, 3) // [1, 2, 3]
 // console.log(numbers)
@@ -53,8 +53,43 @@ let secondFromTheEndItem = 장바구니[장바구니.length - 2];
 let lastCartItem = 장바구니[장바구니.length - 1];
 console.log(firstCartItem, secondFromTheEndItem, lastCartItem);
 
-// .at() 메서드에 인덱스를 전달해 배열 항목 가져오기 Add commentMore actions
+// .at() 메서드에 인덱스를 전달해 배열 항목 가져오기
 let firstCartItem2 = 장바구니.at(0);
 let secondFromTheEndItem2 = 장바구니.at(-2);
 let lastCartItem2 = 장바구니.at(-1);
 console.log(firstCartItem2, secondFromTheEndItem2, lastCartItem2);
+
+// 배열 원소(항목) 값 읽기
+console.log(장바구니[2]);
+console.log(장바구니.at(2));
+
+// 배열 원소 값 수정
+장바구니[2] = '순두부';
+console.log(장바구니[2]);
+
+// 1. 빈 배열을 생성합니다.
+const emptyArray = [];
+// 2. 3개의 항목을 포함하는 배열을 작성합니다.
+const threeArray = [true, undefined, null];
+const instructors = ['야무', '슬비'];
+// 3. `instructors` 배열에서 `"야무"` 항목을 가져와 `yamoo9` 변수에 할당합니다.
+let yamoo9 = instructors[0];
+console.log(yamoo9);
+// 4. `instructors` 배열에서 `"슬비"` 항목 값을 `"김데레사"`로 변경합니다.
+instructors[1] = '김데레사';
+console.log(instructors);
+
+// 배열에서 2개 이상의 값을 꺼내고 싶을 때
+// 장바구니2[0]과 장바구니2[1]을 같이 꺼내고 싶다면 어떻게 할까요?
+const 장바구니2 = ['빵', '과자', '햄스터', '고양이'];
+
+// 초급 수준에서의 답변
+let 첫번째_상품, 두번째_상품;
+
+첫번째_상품 = 장바구니2[0];
+두번째_상품 = 장바구니2.at(1);
+
+console.table({ 첫번째_상품, 두번째_상품 });
+
+// ES2015(6) 수준에서의 답변
+// 구조 분해 할당(Destructuring Assignment)
