218bb98fbe8084361d8e45d1e65b96ee3c51b7b6
jeonjyeon <peacheese17@gmail.com>
2025-06-23 10:02:02 +0900

    [study] 여러 요소 선택 방법 - while - for - for ... of - NodeList.forEach(value, key, parent)

--- assets/scripts/learn/35-select-multiple-element.js
+++ assets/scripts/learn/35-select-multiple-element.js
@@ -34,8 +34,58 @@
   // console.log(navLinksArray.toReversed)
   // console.log(navLinksArray.filter)
 };
+
 (() => {
-  // Node vs. ElementNode
-  // HTML -> DOM의 모든 것은 Node입니다.
-  // 여러 종류의 Node 중, 우리가 가장 많이 사용하는 노드는 요소 노드(ElementNode)입니다.
+  const lifeTips = document.querySelector('.life-tips');
+  // console.log(lifeTips)
+
+  // Document.querySelectorAll(selectors)
+  // Element.querySelectorAll(selectors)
+  const paragraphs = lifeTips.querySelectorAll('p');
+  // console.log(paragraphs)
+
+  // const firstParagraphElement = paragraphs.item(0)
+  // console.log(firstParagraphElement)
+  // const secondParagraphElement = paragraphs.item(1)
+  // console.log(secondParagraphElement)
+  // const lastParagraphElement = paragraphs.item(paragraphs.length - 1)
+  // console.log(lastParagraphElement)
+
+  const PRIMARY_KEY = 'color-primary';
+
+  // Loop (반복, 순환)
+  // - while
+  let i = 0,
+    l = paragraphs.length;
+  while (i < l) {
+    const paragraphElement = paragraphs.item(i);
+    paragraphElement.classList.add(PRIMARY_KEY);
+    ++i;
+  }
+
+  // - do...while
+
+  // - for
+  for (let i = 0, l = paragraphs.length; i < l; ++i) {
+    const paragraphElement = paragraphs.item(i);
+    // console.log(paragraphElement)
+    if (paragraphElement.classList.contains(PRIMARY_KEY)) {
+      paragraphElement.classList.remove(PRIMARY_KEY);
+    }
+  }
+
+  // - for...of
+  for (const paragraphElement of paragraphs) {
+    // console.log(paragraphElement)
+    if (!paragraphElement.classList.contains(PRIMARY_KEY)) {
+      paragraphElement.classList.add(PRIMARY_KEY);
+    }
+  }
+
+  // - NodeList.forEach((value, key, parent) => {...})
+  paragraphs.forEach((p) => {
+    if (p.classList.contains(PRIMARY_KEY)) {
+      p.classList.remove(PRIMARY_KEY);
+    }
+  });
 })();
