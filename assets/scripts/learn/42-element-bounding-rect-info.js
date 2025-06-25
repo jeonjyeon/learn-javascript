4186fca6379be8b46f985d0b194001541d5e6f48
jeonjyeon <peacheese17@gmail.com>
2025-06-25 11:22:10 +0900

    [study] 요소의 수치 및 위치 정보

---
+++ assets/scripts/learn/42-element-bounding-rect-info.js
@@ -0,0 +1,16 @@
+{
+  const headlineElement = document.querySelector('h1');
+  const headlineRecInfo = headlineElement.getBoundingClientRect();
+
+  // 수치 정보
+  console.log(headlineRecInfo.width);
+  console.log(headlineRecInfo.height);
+
+  // 좌표 정보
+  console.log(headlineRecInfo.x);
+  console.log(headlineRecInfo.left);
+  console.log(headlineRecInfo.y);
+  console.log(headlineRecInfo.top);
+  console.log(headlineRecInfo.right);
+  console.log(headlineRecInfo.bottom);
+}
