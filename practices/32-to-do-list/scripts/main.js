4cdcb7e4159b898031f22d4096d034115492fe86
jeonjyeon <peacheese17@gmail.com>
2025-07-11 11:29:20 +0900

    [practice] 할 일 목록 만들기 2 - DOMPurify 라이브러리 CDN 호출 - DOMPurify.sanitize() 사용 사용자 입력 코드 살균 처리

--- practices/32-to-do-list/scripts/main.js
+++ practices/32-to-do-list/scripts/main.js
@@ -1,3 +1,5 @@
+/* global DOMPurify */
+
 (() => {
   const todoListForm = document.querySelector('.todolist');
   const todoList = document.querySelector('.todolist__tasks');
@@ -43,7 +45,7 @@
 
     // 생성할 할 일 요소의 템플릿 구성 후,
     // innerHTML 속성으로 요소에 HTML 생성
-    taskElement.innerHTML = `
+    taskElement.innerHTML = DOMPurify.sanitize(`
       <input type="checkbox" id="${uniqueId}" />
       <label for="${uniqueId}">
         <span class="task__checkmark" aria-hidden="true">
@@ -58,7 +60,7 @@
           <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
         </svg>
       </button>
-    `;
+    `);
 
     // 생성된 새 할 일 요소 반환
     return taskElement;
