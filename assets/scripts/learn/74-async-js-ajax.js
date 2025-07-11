d5f8f440e1aa59e884bd39945bc72bfe4396253c
jeonjyeon <peacheese17@gmail.com>
2025-07-11 17:22:50 +0900

    [practice]  비동기 자바스크립트 - 연습 - GitHub 사용자의 팔로워 정보 출력

--- assets/scripts/learn/74-async-js-ajax.js
+++ assets/scripts/learn/74-async-js-ajax.js
@@ -2,7 +2,7 @@
 // 📌 AJAX란?
 // --------------------------------------------------------------------------
 
-(() => {
+() => {
   // XMLHttpRequest 생성자 함수(클래스)를 사용해 XMLHttpRequest 객체(인스턴스) 생성
   // 요청을 위한 XHR 객체 생성
   const request = new XMLHttpRequest();
@@ -21,4 +21,45 @@
 
   // 서버로 요청 보내기
   request.send();
+};
+
+(() => {
+  function requestGitHubUserFollowers(userName) {
+    // https://api.github.com/users/{username}
+    // ...
+
+    // 1. XHR 객체 생성
+    const request = new XMLHttpRequest();
+
+    // 2. 요청 구성
+    request.open('GET', `https://api.github.com/users/${userName}/followers`);
+
+    // 3. 요청 전송
+    request.send(null);
+
+    // 4. 응답 이벤트 리스너 설정
+    request.addEventListener('load', (e) => {
+      const xhr = e.target;
+      console.log(`응답 상태 코드 = ${xhr.status}`);
+      console.log(`응답 데이터 = ${xhr.response}`);
+
+      // 문자 값 응답 -> JSON 문자열 -> JavaScript 객체 반환
+      const responseData = JSON.parse(xhr.response);
+      // console.log(responseData);
+
+      // 로그인(login) 데이터만 새롭게 배열 재구성
+      const followers = responseData.map(({ login }) => login);
+      // console.log(followers);
+
+      const followerItemsTemplate = followers
+        .map((name) => {
+          return `<li>${name}</li>`;
+        })
+        .join('');
+
+      document.querySelector('.github-followers').innerHTML = followerItemsTemplate;
+    });
+  }
+
+  requestGitHubUserFollowers('jeonjyeon');
 })();
