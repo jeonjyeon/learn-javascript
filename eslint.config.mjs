36106a4ad4becb48180237f42ede37e7a537c70d
jeonjyeon <peacheese17@gmail.com>
2025-07-17 23:07:20 +0900

    [study] JavaScript (ES) 모듈 - 모듈 타입 지정 - 모듈 내보내기 - 모듈 불러오기

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'module' } },
   { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } },
   { files: ['**/*.md'], plugins: { markdown }, language: 'markdown/commonmark', extends: ['markdown/recommended'] },
   { files: ['**/*.css'], plugins: { css }, language: 'css/css', extends: ['css/recommended'] },
